67f1fb469ca32b05d1c41afcf9a5eeb729e261d5
Isaiah Elston <156853632+isaiah-elston@users.noreply.github.com>
2024-03-06 03:29:40 +0000

    Testing my implementation

--- code.js
+++ code.js
@@ -1,3 +1,41 @@
+function swap(array, left, right) {
+    var temp = array[left];
+    array[left] = array[right];
+    array[right] = temp;
+}
+
+function qsort(array, left, right) { 
+    if (left == right) {
+        return;
+    } else {
+        let p = left;
+        for (let i = (left + 1); i <= right; i++) {
+            if (array[i] < array[left]) {
+                swap(array, ++p, i);
+            }
+        }
+        swap(array, left, p);
+        return p;
+    }
+}
+
 function quicksort(array) {
+    if (array.length <= 1) {
+        return array;
+    } else {
+        let left = 0;
+        let right = array.length;
+        while (left < right) {
+            let p = qsort(array, left, right);
+                if (p == left) {
+                p = qsort(array, ++left, right);
+            } else if (p == right) {
+                p = qsort(array, left, --right);
+            } else {
+                qsort(array, left, --p);
+                qsort(array, ++p, right);
+            }
+        }
+    }
     return array;
 }

fc4ffc4fb3ed0a235476d7cd790aad5ccbaf37fa
Isaiah Elston <ielston@uwyo.edu>
2024-03-08 21:30:49 -0700

    I ret-conned back to how I was approaching the problem in attempt #1...

--- code.js
+++ code.js
@@ -1,42 +1,37 @@
 function swap(array, left, right) {
-    var temp = array[left];
-    array[left] = array[right];
-    array[right] = temp;
+	var temp = array[left];
+	array[left] = array[right];
+	array[right] = temp;
 }
 
-function qsort(array, left, right) { 
-    if (left == right || left > right) {
-        return;
-    } else {
-        let p = left;
-        for (let i = (left + 1); i <= right; i++) {
-            if (array[i] < array[left]) {
-                swap(array, ++p, i);
-            }
-        }
-        swap(array, left, p);
-        return p;
-    }
+function qsort(array, left, right) {
+	let p = left;
+	for (let i = left + 1; i <= right; i++) {
+		if (array[i] < array[left]) {
+			swap(array, ++p, i);
+		}
+	}
+	swap(array, left, p);
+	return p;
 }
-
 
 function quicksort(array) {
-    if (array.length <= 1) {
-        return array;
-    } else {
-        let left = 0, right = array.length - 1;
-        let pivot = qsort(array, left, right);
-        let nextRight = --pivot, nextLeft = ++pivot;
-        while (nextRight > left || nextLeft < right) {
-             if (pivot == left) {
-                pivot = qsort(array, left++, right);
-            } else if (pivot == right) {
-                pivot = qsort(array, left, right--);
-            } else {
-                pivot = qsort(array, left, nextRight--);
-                qsort(array, nextLeft++, right);
-            }
-        }
-        return array;
-    }
+	if (array.length <= 1) {
+		return array;
+	} else {
+		let left = 0;
+		let right = array.length - 1;
+		let p = qsort(array, left, right);
+		while (left < right) {
+			p = qsort(array, left, --p);
+			if (p == left) {
+				++left;
+			}
+			p = qsort(array, ++p, right);
+			if (p == right) {
+				--right;
+			}
+		}
+	}
+	return array;
 }
